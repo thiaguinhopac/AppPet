021a50ce0a75bbfb6ec98eec4762c48ba6c09fe2
Thiago <thiupac@bitbucket.org>
2021-04-03 20:28:18 -0300

    Feito telas iniciais sem layout e adicionado codigo de manipulação ao FireBase, tambem criado uma classe que gera Boleto

---
+++ app/src/main/java/com/example/myapplication/screens/payment/SelectPayment.java
@@ -0,0 +1,16 @@
+package com.example.myapplication.screens.payment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.myapplication.R;
+
+public class SelectPayment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_payment);
+    }
+}
